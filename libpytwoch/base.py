d6202ace9635802813d3e7a93d219550fcbcf916
synpey <synpey@gmail.com>
2010-09-23 21:57:37 +0900

    Refactor download() in base.py functionize some code in download() and cleanup code.

--- libpytwoch/base.py
+++ libpytwoch/base.py
@@ -8,85 +8,109 @@ import gzip
 import fcntl
 import re
 
-class Py2chdlerError(Exception):
+class PytwochError(Exception):
     pass
 
 class Base:
+    
+    def download(self, url, file_path):
+        if re.match('^.*\.dat$', url):
+            is_dat_url = True
+        else:
+            is_dat_url = False
+        is_cache_exists = os.path.exists(file_path)
 
-    def http_get(self, url, mtime = None, size=None, compress=True):
-        # create request object
+        cache_mtime = self.get_mtime(file_path)
+        if is_cache_exists and is_dat_url:
+            cache_size = os.path.getsize(file_path) - 1
+        else:
+            cache_size = None
+
+        response = self.http_get(url)
+
+        # check abone and retry if abone
+        if is_dat_url and is_cache_exists:
+            if not response['content'][0].decode('sjis', 'replace') == "\n":
+                response = self.http_get(url, cache_mtime)
+
+        # save cache
+        if response['code'] == 200 or response['code'] == 206:
+            if response['code'] == 206:
+                shutil.copy2(file_path, file_path + "_old")
+            self.__save_cache(response)
+        elif response['code'] == 304:
+        else:
+            raise Py2chdlerError("HTTPERROR:" + code + ": Can't access " + url
+                                 + ".")
+
+        # make return value 
+        if response['code'] == 200 or response['code'] == 206 or\
+           response['code'] == 304:
+            ret_val = { "code"   : response['code'],
+                        "content": response['content'][1-] }
+        else:
+            ret_val = { "code"   : response['code'],
+                        "content": None }
+        return ret_val
+
+
+    def __save_cache(self, response)
+        if response['code'] == 200:
+            self.write_file(file_path, response['content'])
+        if response['code'] == 206:
+            self.write_file(file_path, response['content'], True)
+        self.set_mtime(filepath, 
+                       self.convert_time_format(response['last-modified']))
+
+
+    def __http_get(self, url, mtime, byte_from):
         request = urllib.request.Request(url)
-        # add headers
+
+        # add header
         if mtime:
             request.add_header('If-Modified-Since', mtime)
-        if size:
-            request.add_header('Range', "bytes=" + str(size) + "-")
-        elif compress:
+        if byte_from:
+            request.add_header('Range', "bytes=" + str(byte_from) + "-")
+        else:
+            is_compressed = True
             request.add_header('Accept-Encoding', 'deflate, gzip')
-        request.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6 (.NET CLR 3.5.30729)')
+        request.add_header('User-Agent',
+                           'Mozilla/5.0 (Windows; U; Windows NT 5.1; '
+                           'en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6 '
+                           '(.NET CLR 3.5.30729)')
+
+        # open request
         opener = urllib.request.build_opener()
-        # Catch HTTPError as set code
         try:
-            remote_file = opener.open(request)
-            code = remote_file.code
+            response = opener.open(request)
+            response_coode = response.code
         except urllib.error.HTTPError as e:
-            code = e.code
-        # if access success, read data
-        if code == 200 or code == 206:
-            etag = remote_file.headers['etag']
-            last_modified = remote_file.headers['last-modified']
-            content_length = remote_file.headers['content-length']
-            content = remote_file.read()
-            remote_file.close()
+            response_code = e.code
+
+        # if success process data
+        if response_code == 200 or response_code == 206:
+            content = response.read()
+            response.close()
             # if compressed, decompress data
-            if compress == True and size == None:
-                content_encoding = remote_file.headers['content-encoding']
-                p_zip = re.compile('.*(gzip|deflate).*')
-                r_zip = p_zip.search(content_encoding) if content_encoding else False
-                if r_zip:
+            if is_compressed == True and byte_from == None:
+                if re.search('.*(gzip|deflate).*',
+                             response.headers['content-encoding'])
                     bin = io.BytesIO(content)
                     decompressed = gzip.GzipFile(fileobj=bin, mode="rb")
                     content = decompressed.read()
-        # Set default value as None if download fails
+            response = { "code"             : response_code,
+                         "etag"             : response.headers['etag'],
+                         "last-modified"    : response.headers['last-modified'],
+                         "content-length"   : response.headers['content-length'],
+                         "content"          : content }
         else:
-            code = code; etag = None; last_modified = None; content_length = None; content= None
-        # make data dic
-        response = {"code": code, "etag": etag, "last-modified": last_modified, "content-length": content_length, "content": content}
+            response.close()
+            response = { "code"             : response_code,
+                         "etag"             : None,
+                         "last-modified"    : None,
+                         "content-length"   : None,
+                         "content"          : None }
         return response
-
-    def download(self, url, filepath, err_stop=True):
-        file_existance = os.path.exists(filepath)
-        local_mtime = self.convert_time_format(os.path.getmtime(filepath)) if file_existance else None
-        # pass size to http_get method if request file is dat
-        p_dat = re.compile('^.*\.dat$')
-        r_dat = p_dat.match(url)
-        if r_dat:
-            filesize = os.path.getsize(filepath) - 1 if file_existance else None
-        else:
-            filesize = None
-        # get remote file
-        response = self.http_get(url, local_mtime, filesize)
-        # chk abone
-        if response['code'] == 206:
-            content = response['content'].decode('sjis', 'replace')
-            if not content[0] == "\n":
-                response = self.http_get(url, local_mtime)
-        # get response code
-        code = response['code']
-        if code == 200 or code == 206 or code == 304:
-            if file_existance:
-                self.copy_file(filepath)
-            if code == 200 or code == 206:
-                if file_existance:
-                    self.copy_file(filepath)
-                # add to file if response code eq 206
-                add = True if code == 206 else False
-                self.write_file(filepath, response['content'], add)
-                remote_mtime = self.convert_time_format(response['last-modified'])
-                self.set_mtime(filepath, remote_mtime)
-        elif err_stop == True:
-            raise Py2chdlerError("HTTPERROR: Can't access " + url +". The response code is: " + str(code))
-        return code
 
 
     def write_file(self, filepath, data, add=False):
@@ -99,6 +123,7 @@ class Base:
         fcntl.flock(fs.fileno(), fcntl.LOCK_UN)
         fs.close
 
+
     def read_file(self, filepath):
         fs = io.open(filepath, mode='rb')
         fcntl.flock(fs.fileno(), fcntl.LOCK_SH)
@@ -109,6 +134,7 @@ class Base:
         lines = data.splitlines()
         return lines
 
+
     def read_raw_file(self, filepath):
         fs = io.open(filepath, mode='rb')
         fcntl.flock(fs.fileno(), fcntl.LOCK_SH)
@@ -117,16 +143,18 @@ class Base:
         fs.close()
         return data
 
-    def copy_file(self, from_filepath):
-        to_filepath = from_filepath + "_old"
-        shutil.copy2(from_filepath, to_filepath)
-
-    def set_mtime(self, filepath, mtime):
+    def set_mtime(self, file_path, mtime):
         atime = time.time()
         times = (atime, mtime)
-        stat = os.utime(filepath, times)
-    
-    def convert_time_format(self, inp_time , change_tz=True):
+        stat = os.utime(file_path, times)
+
+    def get_mtime(file_path):
+        mtime = None
+        if os.path.exists(file_path):
+            mtime = self.convert_time_format(os.path.gettime(file_path))
+        return mtime
+
+    def convert_time_format(self, inp_time, change_tz=True):
         if isinstance(inp_time, str):
             time_struct = time.strptime(inp_time, "%a, %d %b %Y %H:%M:%S %Z")
             ret_time = int(time.mktime(time_struct) + 32400)
@@ -134,6 +162,8 @@ class Base:
             time_struct = time.gmtime(inp_time)
             ret_time = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time_struct)
         return ret_time
+
+
 
 if __name__ == '__main__':
     base = Base()
--- pytwoch.py
+++ pytwoch.py
@@ -1,41 +1,32 @@
 import os, os.path
-from libpy2chdler import bbsmenu
+from libpytwoch import menu
 
 class Py2chdler:
-    def __init__(self, base_dir, bbsmenu_url):
-        self.settings = {'base_dir': base_dir}
-        self.bbsmenu = Bbsmenu(self.settings, bbsmenu_url)
-        # check rights to base_dir
-        if os.path.isdir(base_dir) == False or os.access(base_dir, os.W_OK) == False:
-            raise Py2chdlerError(base_dir + " not exists or not writable")
+    def __init__(self, cache_dir_path, cache_maintain_span=90):
+        # Check parameters.
+        ## set error_msg to None
+        error_msg = None
+        ## Check right to the cache_dir_path
+        if os.path.isdir(cache_dir_path) == False or \
+           os.access(cache_dir_path, os.W_OK) == False:
+            error_msg = cache_dir_path + ' is not exist or not writable'
+
+        ## Check if cache_maintain_span is numeric and is between 0 to 730.
+        if not isinstance(1,int):
+            error_msg = 'Cache maintain span must be numeric and must be fall '+\
+            'in 0 to 730'
+
+        ## raise error if errormsg
+        if error_msg:
+            rase PytwoError(error_msg)
+
+        # save config
+        self.config = { 'cache_dir_path'        : cache_dir_path,
+                        'cache_maintain_span'   : cache_maintain_span }
+
 
     # methods to construct 2ch objects
-    def get_bbsmenu(self):
-        return self.bbsmenu;
-    def get_boards(self, *board_names):
-        return self.bbsmenu.get_boards(board_names)
-    def get_board(self, board_name):
-        return self.bbsmenu.get_board(board_name)
-#    # args are tuple of "board_name" and "thread_id"
-#    def get_threads(self, *args):
-#        pass
-#    def get_thread(self, board_name, thread_id):
-#        pass
-#    def get_new_thread(self):
-#        pass
-#    def get_reses(self, *args):
-#        pass
-#    # args are tuple of "board_name", "thread_id", and "res_id"
-#    def get_res(self, board_name, thread_id, res_id):
-#        pass
-#    def get_new_reses(self):
-#        pass
-
+    def get(self, url):
+        pass
 
 if __name__ == '__main__':
-    py2chdler = Py2chdler(os.path.abspath('data'), 'http://menu.2ch.net/bbsmenu.html')
-    bbsmenu = py2chdler.get_bbsmenu()
-    boards = bbsmenu.get_boards('megami', 'neet4pink')
-    for board in boards:
-        print(board.board_name + board.board_name_alphabet + board.board_url)
-    bbsmenu.download()
